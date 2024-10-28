3341623867535ce8ad63b2956b7454b8aceaf30c
leon121271 <don.leon121271@gmail.com>
2024-10-28 21:07:49 +0300

    Добавлен новый файл

---
+++ module_1_4..py
@@ -0,0 +1,5 @@
+total_tasks = "12"
+hours_spent = "1.5"
+name = "Course:Python"
+lead_time = (1.5 / 12)
+print(name,"total_tasks:",total_tasks,"hours_spent:",hours_spent,"lead_time:",lead_time,"hours")
